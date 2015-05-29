acd03ea12af7000c654e7679db90d96d0c443035
Tommy Shiou <tshiou@turn.com>
2015-05-29 15:13:44 -0700

    Cite BSD open-source license in copyright heading
    Signed-off-by: Tommy Shiou <tshiou@turn.com>

--- src/main/java/com/turn/sorcerer/executor/PipelineExecutor.java
+++ src/main/java/com/turn/sorcerer/executor/PipelineExecutor.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2015 Turn Inc. All Rights Reserved.
- * Proprietary and confidential.
+ * Copyright (C) 2015, Turn Inc.
+ * All Rights Reserved.
  */
 
 package com.turn.sorcerer.executor;
