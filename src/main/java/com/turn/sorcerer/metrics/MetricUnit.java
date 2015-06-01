e39e5de02715e04030fefbcbcf029c04c9fb6bae
Tommy Shiou <tshiou@turn.com>
2015-06-01 14:06:56 -0700

    Updated copyright licensing
    Signed-off-by: Tommy Shiou <tshiou@turn.com>

--- src/main/java/com/turn/sorcerer/metrics/MetricUnit.java
+++ src/main/java/com/turn/sorcerer/metrics/MetricUnit.java
@@ -1,6 +1,7 @@
-/**
- * Copyright (C) 2015 Turn Inc. All Rights Reserved.
- * Proprietary and confidential.
+/*
+ * Copyright (c) 2015, Turn Inc. All Rights Reserved.
+ * Use of this source code is governed by a BSD-style license that can be found
+ * in the LICENSE file.
  */
 
 package com.turn.sorcerer.metrics;
