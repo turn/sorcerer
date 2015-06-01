e885932852fc12d2f2640b7367b3c2ef416057db
Tommy Shiou <tshiou@turn.com>
2015-06-01 14:41:41 -0700

    Update Depdendency java doc
    Signed-off-by: Tommy Shiou <tshiou@turn.com>

--- src/main/java/com/turn/sorcerer/dependency/Dependency.java
+++ src/main/java/com/turn/sorcerer/dependency/Dependency.java
@@ -6,9 +6,30 @@
 
 package com.turn.sorcerer.dependency;
 
+/**
+ * Represents a task dependency
+ *
+ * <p>
+ * Should be implemented by any class that represents a task dependency. A task
+ * can require multiple Dependency objects to be fulfilled.
+ * </p>
+ *
+ * @author tshiou
+ * @see     com.turn.sorcerer.task.Task
+ * @see     com.turn.sorcerer.task.executable.ExecutableTask#checkDependencies()
+ */
 public interface Dependency {
 
+	/**
+	 * Checks if the required dependency is fulfilled
+	 *
+	 * <p>
+	 * Implementation of this method should represent a dependency that a task
+	 * requires.
+	 * </p>
+	 *
+	 * @param iterNo Current iteration number
+	 * @return Return true if dependency requirement is fulfilled
+	 */
 	boolean check(int iterNo);
-
-	String toString();
 }
