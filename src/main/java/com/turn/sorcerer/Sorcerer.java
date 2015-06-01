6a90c3c725964b20a3b36a0db29e81fe811eb635
Tommy Shiou <tshiou@turn.com>
2015-06-01 16:37:10 -0700

    Update Sorcerer class javadocs
    Signed-off-by: Tommy Shiou <tshiou@turn.com>

--- src/main/java/com/turn/sorcerer/Sorcerer.java
+++ src/main/java/com/turn/sorcerer/Sorcerer.java
@@ -18,10 +18,6 @@ import com.turn.sorcerer.status.Status;
 import com.turn.sorcerer.status.StatusManager;
 import com.turn.sorcerer.task.type.TaskType;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -31,12 +27,12 @@ import java.util.concurrent.Executors;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
-import com.google.inject.BindingAnnotation;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 /**
- * Class Description Here
+ * Represents a Sorcerer instance and provides the public API's to the
+ * Sorcerer module
  *
  * @author tshiou
  */
@@ -45,21 +41,43 @@ public class Sorcerer {
 	private static final Logger logger =
 			LogManager.getFormatterLogger(Sorcerer.class);
 
+	/**
+	 * Sorcerer injector containing bindings for all Sorcerer member variables
+ 	 */
 	private final SorcererInjector injector;
 
+	/**
+	 * List of pipeline scheduling threads
+	 */
+	private List<ExecutorService> pipelineThreads = Lists.newArrayList();
+
+	/**
+	 * Private constructor to prevent public instantiation.
+	 * Should only be used by the Sorcerer builder.
+	 *
+	 * @param injector An instance of a Sorcerer Injector will be
+	 *                 used to inject all member variables.
+	 */
 	private Sorcerer(SorcererInjector injector) {
 		this.injector = injector;
 	}
 
-	private List<ExecutorService> pipelineThreads = Lists.newArrayList();
+	/**
+	 * Starts the Sorcerer service by instantiating pipeline scheduling threads
+	 * and submitting them.
+	 */
+	public void start() {
+		logger.info("Starting Sorcerer");
 
-	public void run() throws SorcererException {
-		scheduleAndRunPipeline();
+		scheduleAndRunPipelines();
 	}
 
-	private void scheduleAndRunPipeline() {
-
-		logger.info("Starting Sorcerer");
+	/**
+	 * Creates instances of
+	 * {@link com.turn.sorcerer.executor.PipelineScheduler} and submits them
+	 * for execution. A Single thread per scheduler.
+	 */
+	private void scheduleAndRunPipelines() {
 
 		for (PipelineType pipeline: injector.getPipelines()) {
 
@@ -77,16 +95,25 @@ public class Sorcerer {
 			pipelineThreads.add(pipelineThread);
 		}
 
-		logger.debug("Exiting Sorcerer startup thread");
+		logger.debug("Exiting Sorcerer scheduling thread");
 	}
 
+	/**
+	 * Stops the Sorcerer service by initiating the shutdown of all pipeline
+	 * scheduling threads.
+	 */
 	public void stop() {
 		for (ExecutorService thread : pipelineThreads) {
 			thread.shutdown();
 		}
 	}
 
-	// Return pipelines in the module
+	/**
+	 * Provides a List of pipelines configured to be scheduled by this instance
+	 * of a Sorcerer module
+	 *
+	 * @return Returns a List of pipelines
+	 */
 	public Collection<String> getPipelines() {
 
 		Set<PipelineType> pipelines = injector.getPipelines();
@@ -103,11 +130,30 @@ public class Sorcerer {
 		return pipelineNames;
 	}
 
+	/**
+	 * Provides the current iteration number of a pipeline.
+	 *
+	 * @see     com.turn.sorcerer.pipeline.Pipeline#getCurrentIterationNumber()
+	 * @param   pipelineName Name of pipeline
+	 * @return  Current iteration number of pipeline
+	 */
 	public int getCurrentIterNoForPipeline(String pipelineName) {
 		Pipeline p = injector.getInstance(injector.getPipelineType(pipelineName));
 		return p.getCurrentIterationNumber();
 	}
 
+	/**
+	 * Builds and provides the workflow DAG of a pipeline
+	 *
+	 * <p>
+	 * This method recursively builds the workflow DAG of a pipeline. This is
+	 * represented by a Map of a task name as the key and a list of next task
+	 * names as the value.
+	 * </p>
+	 *
+	 * @param pipelineName Name of pipeline
+	 * @return Workflow DAG of pipeline represented by a Map
+	 */
 	public Map<String, List<String>> getTasksForPipeline(String pipelineName) {
 		PipelineType pipelineType = injector.getPipelineType(pipelineName);
 
@@ -118,6 +164,9 @@ public class Sorcerer {
 		return map;
 	}
 
+	/**
+	 * Recursively build the workflow DAG
+	 */
 	private void recursiveBuildGraph(Map<String, List<String>> graph, String taskName) {
 		if (taskName == null) {
 			return;
@@ -144,26 +193,61 @@ public class Sorcerer {
 		}
 	}
 
+	/**
+	 * Provides the status of a task
+	 *
+	 * @param taskName  Name of task to check status
+	 * @param iterNo    Iteration number of task to check status
+	 * @return          Status of task (See {@link Status})
+	 */
 	public Status getTaskStatus(String taskName, int iterNo) {
 		return StatusManager.get().checkTaskStatus(taskName, iterNo);
 	}
 
+	/**
+	 * Provides the status of a pipeline
+	 *
+	 * @param pipelineName Name of pipeline to check status
+	 * @param iterNo        Iteration number of pipeline to check status
+	 * @return              Status of pipeline (See {@link Status})
+	 */
 	public Status getPipelineStatus(String pipelineName, int iterNo) {
 		return StatusManager.get().checkPipelineStatus(pipelineName, iterNo);
 	}
 
+	/**
+	 * Sets the status of a task
+	 *
+	 * @param taskName  Name of task to set status
+	 * @param iterNo    Iteration number of task to set status
+	 * @param status    Status to set (See {@link Status})
+	 */
 	public void setTaskStatus(String taskName, int iterNo, Status status) {
 		TaskType type = injector.getTaskType(taskName);
 
 		StatusManager.get().commitTaskStatus(type, iterNo, status);
 	}
 
+	/**
+	 * Sets the status of a pipeline
+	 *
+	 * @param pipelineName  Name of pipeline to set status
+	 * @param iterNo        Iteration number of pipeline to set status
+	 * @param status        Status to set
+	 */
 	public void setPipelineStatus(String pipelineName, int iterNo, Status status) {
 		PipelineType type = injector.getPipelineType(pipelineName);
 
 		StatusManager.get().commitPipelineStatus(type, iterNo, status);
 	}
 
+	/**
+	 * Executes a single task
+	 *
+	 * @param taskName      Name of task to execute
+	 * @param iterNo        Iteration number of task to execute
+	 * @param arguments     Map of arguments to pass to task
+	 */
 	public void runTask(String taskName, int iterNo, Map<String, String> arguments) {
 		TaskType type = injector.getTaskType(taskName);
 
@@ -172,6 +256,15 @@ public class Sorcerer {
 		Executors.newSingleThreadExecutor().submit(executor);
 	}
 
+	/**
+	 * Executes a pipeline
+	 *
+	 * @param pipelineName  Name of pipeline to execute
+	 * @param iterNo        Iteration number of pipeline to execute
+	 * @param argMap        Map of task arguments
+	 * @param overwrite     If set to true then Sorcerer will execute tasks that
+	 *                         have already been completed
+	 */
 	public void runPipeline(String pipelineName, int iterNo,
 	                        Map<String, Map<String, String>> argMap, boolean overwrite) {
 		PipelineType type = injector.getPipelineType(pipelineName);
@@ -183,13 +276,45 @@ public class Sorcerer {
 		Executors.newSingleThreadExecutor().submit(executor);
 	}
 
+	/**
+	 * Main method for starting up a standalone instance of Sorcerer
+	 *
+	 * @param args Arguments
+	 *
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception{
+		Sorcerer module = Sorcerer.builder()
+				.addConfigPath(args[0])
+				.create();
+		module.start();
+	}
+
+	/**
+	 * Get a <i>new</i> builder instance for a Sorcerer module
+	 */
 	public static SorcererModuleBuilder builder() {
 		return new SorcererModuleBuilder();
 	}
 
+	/**
+	 * Builds and initializes the Sorcerer module
+	 *
+	 * <p>
+	 * The Sorcerer builder provides an API for building a Sorcerer instance
+	 * and also takes care of the Sorcerer module initialization. If any
+	 * initialization steps fail, a Sorcerer module instance will not be
+	 * returned and instead a SorcererException will be thrown. This will
+	 * prevent providing an invalid instance of a Sorcerer module.
+	 *
+	 * </p>
+	 */
 	public static class SorcererModuleBuilder {
 
+		// List of java packages to scan for Sorcerer object implementations
 		private List<String> pkgs = Lists.newArrayList();
+
+		// List of configuration file paths
 		private List<String> configs = Lists.newArrayList();
 
 		public SorcererModuleBuilder addPackage(String pkg) {
@@ -203,6 +328,8 @@ public class Sorcerer {
 		}
 
 		public Sorcerer create() throws SorcererException {
+			logger.info("Configuring sorcerer module");
+
 			return new Sorcerer(
 					SorcererConfiguration.get()
 							.addConfigs(configs)
@@ -211,16 +338,4 @@ public class Sorcerer {
 		}
 	}
 
-	public static void main(String[] args) throws Exception{
-		logger.info("Configuring sorcerer module");
-		Sorcerer module = Sorcerer.builder()
-				.addConfigPath(args[0])
-				.create();
-		module.run();
-	}
-
-	@Retention(RetentionPolicy.RUNTIME)
-	@Target({ElementType.FIELD, ElementType.PARAMETER})
-	@BindingAnnotation
-	public @interface Pipelines {}
 }
