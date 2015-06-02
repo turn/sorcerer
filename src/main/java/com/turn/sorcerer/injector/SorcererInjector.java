585c1c2e7e896de72bd8126ca5fcb327f19c08e6
Tommy Shiou <tshiou@turn.com>
2015-06-02 13:26:33 -0700

    Add javadocs to SorcererInjector methods
    Signed-off-by: Tommy Shiou <tshiou@turn.com>

--- src/main/java/com/turn/sorcerer/injector/SorcererInjector.java
+++ src/main/java/com/turn/sorcerer/injector/SorcererInjector.java
@@ -17,6 +17,7 @@ import com.turn.sorcerer.util.email.EmailType;
 
 import java.util.Set;
 
+import com.google.inject.ConfigurationException;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Key;
@@ -34,74 +35,184 @@ public class SorcererInjector {
 	private static final Logger logger =
 			LogManager.getFormatterLogger(SorcererInjector.class);
 
+	// We use Guice as the underlying binder
+	private Injector INJECTOR;
+
+	/**
+	 * Singleton pattern. A single instance of Sorcerer runs per JVM.
+	 */
+	private static SorcererInjector INSTANCE = new SorcererInjector();
+
+	public static SorcererInjector get() {
+		return INSTANCE;
+	}
+
+	// Private constructor to prevent instantiation
+	private SorcererInjector() {}
+
 	private final TypeLiteral<Set<PipelineType>> setOfPipelines =
 			new TypeLiteral<Set<PipelineType>>() {};
 
-	private static SorcererInjector INSTANCE = new SorcererInjector();
-
-	private Injector INJECTOR;
-
+	/**
+	 * Configure the SorcererInjector. Should be called once per instance of
+	 * Sorcerer.
+	 *
+	 * <p>
+	 * If the underlying injector is configured and created, this method will
+	 * log a warning and return without doing anything. Sorcerer currently does
+	 * not support reconfiguration.
+	 * </p>
+	 *
+	 * @param module SorcererAbstractModule defining the bindings
+	 */
 	public void configure(SorcererAbstractModule module) {
 
 		if (INJECTOR != null) {
-			logger.error("Injector configuration method called but injector is already configured");
+			logger.warn("Injector configuration method called but injector is already configured");
 			return;
 		}
 
 		INJECTOR = Guice.createInjector(module);
 	}
 
-	public static SorcererInjector get() {
-		return INSTANCE;
+	/**
+	 * Provides the task type registered to a specific task name
+	 *
+	 * <p>
+	 * Uses the underlying injector to get the task type instance binding with
+	 * the provided task name. <b>If the binding is not found the method will
+	 * return null.</b> Therefore the client should handle the case if the
+	 * binding doesn't exist.
+	 * </p>
+	 *
+	 * @param name Name of task
+	 * @return Returns TaskType instance registered to the provided task name.
+	 * Returns null if no task type was found with the provided name.
+	 */
+	public TaskType getTaskType(final String name){
+		try {
+			return INJECTOR.getInstance(
+					Key.get(TaskType.class, Names.named(name)));
+		} catch (ConfigurationException ce) {
+			logger.error(ce);
+			return null;
+		}
 	}
 
-	private SorcererInjector() {}
-
-
-	public TaskType getTaskType(final String name) {
-		return INJECTOR.getInstance(
-				Key.get(TaskType.class, Names.named(name)));
-	}
-
+	/**
+	 * Provides a Task implementation instance of the task type
+	 *
+	 * <p>
+	 * Uses the underlying injector to get an instance of the Task
+	 * implementation that is registered to the provided task type. If no
+	 * binding is found for the task type, the method will return null.
+	 * This should never happen since Sorcerer does eager task type
+	 * reconciliation at initialization. However, it is possible (though
+	 * unsupported) that some unregistered task type was created outside of
+	 * Sorcerer. In any case, the client should handle the return value of
+	 * null if no Task implementation was found for the task type.
+	 * </p>
+	 *
+	 * @param type Task type
+	 * @return Returns a Task instance registered to the provided task name.
+	 * Returns null if no task implementation was found for the task type.
+	 */
 	public Task getInstance(TaskType type) {
-		return INJECTOR.getInstance(
-				Key.get(Task.class, Names.named(type.getName())));
+		try {
+			return INJECTOR.getInstance(
+					Key.get(Task.class, Names.named(type.getName())));
+		} catch (ConfigurationException ce) {
+			logger.error(ce);
+			return null;
+		}
 	}
 
+	/**
+	 * Provides the pipeline type registered to a specific task name
+	 *
+	 * <p>
+	 * Uses the underlying injector to get the pipeline type instance binding
+	 * with the provided pipeline name. <b>If the binding is not found the
+	 * method will return null.</b> Therefore the client should handle the case
+	 * if the binding doesn't exist.
+	 * </p>
+	 *
+	 * @param name Name of pipeline
+	 * @return Returns PipelineType instance registered to the provided
+	 * pipeline name
+	 */
+	public PipelineType getPipelineType(final String name) {
+		try {
+			return INJECTOR.getInstance(
+					Key.get(PipelineType.class, Names.named(name)));
+		} catch (ConfigurationException ce) {
+			logger.error(ce);
+			return null;
+		}
+	}
+
+	/**
+	 * Provides a Pipeline implementation instance of the pipeline type
+	 *
+	 * <p>
+	 * Uses the underlying injector to get an instance of the Pipeline
+	 * implementation that is registered to the provided pipeline type. If no
+	 * binding is found for the pipeline type, the method will return null.
+	 * This should never happen since Sorcerer does eager pipeline type
+	 * reconciliation at initialization. However, it is possible (though
+	 * unsupported) that some unregistered pipeline type was created outside of
+	 * Sorcerer. In any case, the client should handle the return value of
+	 * null if no Pipeline implementation was found for the pipeline type.
+	 * </p>
+	 *
+	 * @param type pipeline type
+	 * @return Returns a Pipeline instance registered to the provided pipeline
+	 * name. Returns null if no Pipeline implementation was found for the
+	 * pipeline type.
+	 */
+	public Pipeline getInstance(PipelineType type) {
+		try {
+			return INJECTOR.getInstance(
+					Key.get(Pipeline.class, Names.named(type.getName())));
+		} catch (ConfigurationException ce) {
+			logger.error(ce);
+			return null;
+		}
+	}
+
+	/**
+	 * Checks if a Pipeline implementation binding exists for a pipeline type
+	 */
 	public boolean bindingExists(PipelineType type) {
 		return INJECTOR.getExistingBinding(
 				Key.get(Pipeline.class, Names.named(type.getName()))
 		) != null;
 	}
 
-	private boolean bindingExists(final String name) {
-		return INJECTOR.getExistingBinding(
-				Key.get(Pipeline.class, Names.named(name))
-		) != null;
-	}
-
-	public PipelineType getPipelineType(final String name) {
-		return INJECTOR.getInstance(
-				Key.get(PipelineType.class, Names.named(name)));
-	}
-
+	/**
+	 * Provides the set of pipeline types to be scheduled in the module
+	 */
 	public Set<PipelineType> getPipelines() {
 		return INJECTOR.getInstance(Key.get(setOfPipelines));
 	}
 
-	public Pipeline getInstance(PipelineType type) {
-		return INJECTOR.getInstance(
-				Key.get(Pipeline.class, Names.named(type.getName())));
-	}
-
+	/**
+	 * Provides the module type representing the current Sorcerer module
+	 */
 	public ModuleType getModule() {
 		return INJECTOR.getInstance(ModuleType.class);
 	}
 
+	/**
+	 * Provides email information defined in the module
+	 */
 	public EmailType getAdminEmail() {
 		return INJECTOR.getInstance(EmailType.class);
 	}
 
+	/**
+	 * Provides an instance of the storage layer
+	 */
 	public StatusStorage getStorageInstance() {
 		return INJECTOR.getInstance(StatusStorage.class);
 	}
