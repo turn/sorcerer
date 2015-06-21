3f128539b6eaa1392280d3e2878831878a6724ce
Tommy Shiou <tshiou@turn.com>
2015-06-20 22:57:12 -0700

    Implement zookeeper storage using curator
    We use apache curator as the zookeeper manager layer.
    
    Signed-off-by: Tommy Shiou <tshiou@turn.com>

--- src/main/java/com/turn/sorcerer/status/impl/ZookeeperStatusStorage.java
+++ src/main/java/com/turn/sorcerer/status/impl/ZookeeperStatusStorage.java
@@ -6,6 +6,7 @@
 
 package com.turn.sorcerer.status.impl;
 
+
 import com.turn.sorcerer.status.Status;
 import com.turn.sorcerer.status.StatusStorage;
 
@@ -14,11 +15,19 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.util.List;
 
+import com.google.common.base.Joiner;
 import com.google.inject.BindingAnnotation;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.RetryUntilElapsed;
+import org.apache.curator.utils.ZKPaths;
 import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Zookeeper persistent storage layer implementation
@@ -26,6 +35,8 @@ import org.joda.time.DateTime;
  * @author tshiou
  */
 public class ZookeeperStatusStorage implements StatusStorage {
+	private static final Logger logger =
+			LoggerFactory.getLogger(ZookeeperStatusStorage.class);
 
 	private String type = "default";
 
@@ -53,9 +64,27 @@ public class ZookeeperStatusStorage implements StatusStorage {
 	@Named(RETRY_INTERVAL)
 	private Integer retryInterval;
 
+	private CuratorFramework curator;
+	private boolean initialized = false;
+
+	private static final Joiner PATH = Joiner.on('/').skipNulls();
+
 	@Override
 	public void init() throws IOException {
+		logger.debug("Initializing Zookeeper storage: {}", connectionString);
 
+		curator = CuratorFrameworkFactory.newClient(
+				connectionString, sessionTimeout, connectionTimeout,
+				new RetryUntilElapsed(retryDuration, retryInterval)
+		);
+		curator.start();
+		initialized = true;
+
+		try {
+			ZKPaths.mkdirs(curator.getZookeeperClient().getZooKeeper(), ZKPaths.makePath(root, type));
+		} catch (Exception e) {
+			throw new IOException(e);
+		}
 	}
 
 	@Override
@@ -66,37 +95,148 @@ public class ZookeeperStatusStorage implements StatusStorage {
 
 	@Override
 	public DateTime getLastUpdateTime(String identifier, int id) throws IOException {
-		return null;
+
+		String path = PATH.join(root, type, identifier, id);
+
+		try {
+			if (curator.checkExists().forPath(path) == null) {
+				return new DateTime(0);
+			}
+
+			return new DateTime(curator.checkExists().forPath(path).getMtime());
+
+		} catch (Exception e) {
+			throw new IOException(e);
+		}
 	}
 
 	@Override
 	public DateTime getStatusUpdateTime(String identifier, int id, Status status) throws IOException {
-		return null;
+
+		String path = PATH.join(root, type, identifier, id, status);
+
+		try {
+			if (curator.checkExists().forPath(path) == null) {
+				return new DateTime(0);
+			}
+
+			return new DateTime(curator.checkExists().forPath(path).getCtime());
+
+		} catch (Exception e) {
+			throw new IOException(e);
+		}
+
 	}
 
 	@Override
 	public int getCurrentIterNo(String identifier) throws IOException {
-		return 0;
+
+		String path = PATH.join(root, type, identifier);
+
+		int maxIterNo = 0;
+		List<String> children;
+
+		try {
+			if (curator.checkExists().forPath(path) == null) {
+				return 0;
+			}
+
+			children = curator.getChildren().forPath(path);
+
+		} catch (Exception e) {
+			throw new IOException(e);
+		}
+
+		for (String child : children) {
+			try {
+				maxIterNo = Math.max(Integer.parseInt(child), maxIterNo);
+			} catch (NumberFormatException nfe) {
+				continue;
+			}
+		}
+
+		return maxIterNo;
 	}
 
 	@Override
 	public Status checkStatus(String identifier, int id) throws IOException {
-		return null;
+
+		String path = PATH.join(root, type, identifier, id);
+
+		List<String> children;
+
+		try {
+			if (curator.checkExists().forPath(path) == null) {
+				return Status.PENDING;
+			}
+
+			children = curator.getChildren().forPath(path);
+
+		} catch (Exception e) {
+			throw new IOException(e);
+		}
+
+		if (children.contains(Status.SUCCESS.toString())) {
+			return Status.SUCCESS;
+		}
+
+		if (children.contains(Status.IN_PROGRESS.toString())) {
+			return Status.IN_PROGRESS;
+		}
+
+		if (children.contains(Status.ERROR.toString())) {
+			return Status.ERROR;
+		}
+
+		return Status.PENDING;
 	}
 
 	@Override
 	public void clearAllStatuses(String identifier, int jobId) throws IOException {
+		String path = PATH.join(root, type, identifier, jobId);
 
+		try {
+			if (curator.checkExists().forPath(path) == null) {
+				return;
+			}
+
+			curator.delete().forPath(path);
+
+		} catch (Exception e) {
+			throw new IOException(e);
+		}
 	}
 
 	@Override
 	public void removeStatus(String identifier, int jobId, Status status) throws IOException {
 
+		String path = PATH.join(root, type, identifier, jobId, status);
+
+		try {
+			if (curator.checkExists().forPath(path) == null) {
+				return;
+			}
+
+			curator.delete().forPath(path);
+
+		} catch (Exception e) {
+			throw new IOException(e);
+		}
 	}
 
 	@Override
 	public void commitStatus(String identifier, int jobId, Status status, DateTime time, boolean overwrite)
 			throws IOException {
+		String path = PATH.join(root, type, identifier, jobId, status);
+
+		try {
+			ZKPaths.mkdirs(curator.getZookeeperClient().getZooKeeper(), path);
+
+			curator.checkExists().forPath(path).setCtime(time.getMillis());
+
+		} catch (Exception e) {
+			throw new IOException(e);
+		}
 
 	}
 
